e2b25031423b5a7565ac860eee022db5429ec2a7
Hira Kazmi <hirarukh@gmail.com>
2016-06-15 14:40:14 -0400

    created cast seed, ran rake db:seed

--- movie_cast/db/seeds.rb
+++ movie_cast/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Cast.create(
+  movie_title: "The Lord of the Rings",
+  url: "http://cdn1-www.craveonline.com/assets/uploads/gallery/lord-of-the-rings-sucks/lord-of-the-rings-no-main-character.jpg"
+)
+
+Cast.create(
+  movie_title: "Harry Potter",
+  url: "http://1.bp.blogspot.com/-aQ2MGJYw0hI/Tsa8ZscnknI/AAAAAAAADaw/-oEG2PCtbvA/s1600/Untitled-2.jpg"
+)
+
+Cast.create(
+  movie_title: "The Avengers",
+  url: "https://iambobknarwhal.files.wordpress.com/2014/10/the-avengers-collection-512951e49d95a-2.jpg"
+)
